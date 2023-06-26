64a3559467d8b49a0adb81b2c6de28e597a47e11
Craftarc <craftarc0@gmail.com>
2023-06-27 00:26:25 +0800

    Add test directory Update cmakelists to link boost library Add main file (not involved in project yet)

---
+++ test/test1.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE MyTestModule
+
+#include <boost/test/unit_test.hpp>
+
+// Example test case
+BOOST_AUTO_TEST_CASE(test_addition)
+{
+    int result = 1 + 1;
+    BOOST_TEST(result == 2);
+}
+
+// Another test case
+BOOST_AUTO_TEST_CASE(test_subtraction)
+{
+    int result = 5 - 3;
+    BOOST_TEST(result == 2);
+}
