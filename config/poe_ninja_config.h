6860c938fb3cfc39300c9c2f3bc0e5b885001c39
Craftarc <craftarc0@gmail.com>
2023-07-06 15:48:18 +0800

    Add all poe ninja endpoint constants like league names, currency/item type names

--- config/poe_ninja_config.h
+++ config/poe_ninja_config.h
@@ -3,27 +3,51 @@
 
 #include <string>
 
+/**
+ * Contains definitions for all relevant parameters in poe.ninja endpoints
+ * paths: currently available currency/item types
+ * leagues: currently available leagues
+ *
+ * Last updated: Crucible 7/6/2023
+ */
 namespace config {
     namespace poe_ninja {
         const std::string host = "poe.ninja";
-        const std::string currency_path = "/api/data/currencyoverview?league=Crucible&type=Currency";
-        const std::string fragment_path = "/api/data/currencyoverview?league=Crucible&type=Fragment";
-        const std::string oil_path = "/api/data/currencyoverview?league=Crucible&type=Oil";
-        const std::string incubator_path = "/api/data/currencyoverview?league=Crucible&type=Incubator";
-        const std::string scarab_path = "/api/data/currencyoverview?league=Crucible&type=Scarab";
-        const std::string fossil_path = "/api/data/currencyoverview?league=Crucible&type=Fossil";
-        const std::string resonator_path = "/api/data/currencyoverview?league=Crucible&type=Resonator";
-        const std::string essence_path = "/api/data/currencyoverview?league=Crucible&type=Essence";
-        const std::string divination_card_path = "/api/data/currencyoverview?league=Crucible&type=DivinationCard";
-        const std::string prophecy_path = "/api/data/currencyoverview?league=Crucible&type=Prophecy";
-        const std::string skill_gem_path = "/api/data/currencyoverview?league=Crucible&type=SkillGem";
-        const std::string base_type_path = "/api/data/currencyoverview?league=Crucible&type=BaseType";
-        const std::string helmet_enchant_path = "/api/data/currencyoverview?league=Crucible&type=HelmetEnchant";
-        const std::string unique_map_path = "/api/data/currencyoverview?league=Crucible&type=UniqueMap";
-        const std::string map_path = "/api/data/currencyoverview?league=Crucible&type=Map";
-        const std::string unique_jewel_path = "/api/data/currencyoverview?league=Crucible&type=UniqueJewel";
-        const std::string unique_accessory = "/api/data/currencyoverview?league=Crucible&type=UniqueAccessory";
-        const std::string beast_path = "/api/data/currencyoverview?league=Crucible&type=Beast";
+        namespace paths {
+            const std::string currency = "/api/data/currencyoverview?type=Currency";
+            const std::string fragment = "/api/data/currencyoverview?type=Fragment";
+            const std::string divination_card = "/api/data/itemoverview?type=DivinationCard";
+            const std::string artifact = "/api/data/itemoverview?type=Artifact";
+            const std::string oil = "/api/data/itemoverview?type=Oil";
+            const std::string unique_weapon = "/api/data/itemoverview?type=UniqueWeapon";
+            const std::string unique_armour = "/api/data/itemoverview?type=UniqueArmour";
+            const std::string unique_accessory = "/api/data/itemoverview?type=UniqueAccessory";
+            const std::string unique_flask = "/api/data/itemoverview?type=UniqueFlask";
+            const std::string unique_jewel = "/api/data/itemoverview?type=UniqueJewel";
+            const std::string skill_gem = "/api/data/itemoverview?type=SkillGem";
+            const std::string cluster_jewel = "/api/data/itemoverview?type=ClusterJewel";
+            const std::string map = "/api/data/itemoverview?type=Map";
+            const std::string blighted_map = "/api/data/itemoverview?type=BlightedMap";
+            const std::string blight_ravaged_map = "/api/data/itemoverview?type=BlightRavagedMap";
+            const std::string scourged_map = "/api/data/itemoverview?type=ScourgedMap";
+            const std::string unique_map = "/api/data/itemoverview?type=UniqueMap";
+            const std::string delirium_orb = "/api/data/itemoverview?type=DeliriumOrb";
+            const std::string invitation = "/api/data/itemoverview?type=Invitation";
+            const std::string scarab = "/api/data/itemoverview?type=Scarab";
+            const std::string base_type = "/api/data/itemoverview?type=BaseType";
+            const std::string fossil = "/api/data/itemoverview?type=Fossil";
+            const std::string resonator = "/api/data/itemoverview?type=Resonator";
+            const std::string helmet_enchant = "/api/data/itemoverview?type=HelmetEnchant";
+            const std::string beast = "/api/data/itemoverview?type=Beast";
+            const std::string essence = "/api/data/itemoverview?type=Essence";
+            const std::string vial = "/api/data/itemoverview?type=Vial";
+        }
+
+        namespace leagues {
+            const std::string crucible = "Crucible";
+            const std::string sanctum = "Sanctum";
+        }
+
     }
 }
 #endif //AXEL_POE_NINJA_CONFIG_H
