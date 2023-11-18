b0a2a2203bb4b2b0a0d2d1791a765dc6bfcceb5f
Craftarc <craftarc0@gmail.com>
2023-11-18 09:18:28 +0000

    feat: Make exception class print nicer traces

--- src/axel/include/axel/Exception.h
+++ src/axel/include/axel/Exception.h
@@ -6,6 +6,8 @@
 #define BACKWARD_HAS_BFD 1
 #include <backward.hpp>
 
+using namespace backward;
+
 namespace axel {
     class Exception : public std::runtime_error {
         public:
@@ -13,6 +15,14 @@ namespace axel {
 
         private:
             void print_stack_trace();
+
+            /// @brief Prints a single trace (frame).
+            /// @param A single resolved trace.
+            /// @param count The number to give to this frame.
+            void print_frame(ResolvedTrace& trace, size_t number);
+
+            SnippetFactory snippet_;
+            size_t context_size_ = 3;
     };
 }  // namespace axel
 #endif  // AXEL_EXCEPTION_H
--- src/axel/src/Exception.cpp
+++ src/axel/src/Exception.cpp
@@ -1,20 +1,63 @@
 #include "axel/Exception.h"
 
+const std::string GREEN = "\033[32m";
+const std::string RED = "\033[31m";
+const std::string RESET = "\033[0m";
+
 namespace axel {
     Exception::Exception(const std::string& message) :
-        std::runtime_error(message) {
+        std::runtime_error(message),
+        snippet_(SnippetFactory()) {
         print_stack_trace();
     }
 
     void Exception::print_stack_trace() {
+        size_t CUT = 4;  // Last 4 traces are from the Exception class itself
         using namespace backward;
         StackTrace st;
         st.load_here(8);
-        Printer p;
-        p.object = false;
-        p.snippet = true;
-        p.color_mode = ColorMode::always;
-        p.address = true;
-        p.print(st, stderr);
+
+        // Remove useless traces
+        TraceResolver tr;
+        tr.load_stacktrace(st);
+        size_t frame_count{ 1 };
+        std::cout << RED << "Printing stacktrace (Most recent call last)"
+                  << RESET << std::endl;
+        for (size_t i = st.size() - 1; i >= CUT; --i) {
+            ResolvedTrace trace = tr.resolve(st[i]);
+            print_frame(trace, frame_count);
+            frame_count += 1;
+        }
+    }
+
+    void Exception::print_frame(ResolvedTrace& trace, size_t number) {
+        // Print the number of this trace
+        std::cout << GREEN << "#" << number << std::endl << RESET;
+
+        // Print the filename
+        std::cout << "From file: " << GREEN << trace.source.filename << RESET
+                  << std::endl;
+
+        // Print the function name
+        std::cout << "In function call: " << GREEN << trace.source.function
+                  << RESET << std::endl;
+
+        // Print where the function was
+        std::cout << "On line: " << trace.source.line
+                  << ", column: " << trace.source.col << std::endl;
+
+        std::cout << std::endl;
+
+        // Print some context
+        SnippetFactory::lines_t context_list =
+        snippet_.get_snippet(trace.source.filename,
+                             trace.source.line,
+                             context_size_);
+        for (auto& line : context_list) {
+            std::cout << RED << line.first << RESET << " " << line.second
+                      << std::endl;
+        }
+
+        std::cout << "---------------------------" << std::endl << std::endl;
     }
 }  // namespace axel
